da29ac84763d2bb8c0db5f07b19dc76b592533b1
Lyubov Kirpa <kirpa.lyubov.a@gmail.com>
2023-05-19 16:53:38 +0300

    Formatting Prettier

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { data } from '../data/users';
+
+export class App extends Component {
+  state = {
+    users: data,
+  };
+
+  render() {
+    return <div></div>;
+  }
+}
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,5 @@
+
+
+export const Button = ({ text, clickHendler }) => {
+  return <button type="button">{text}</button>;
+};
